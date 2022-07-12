082681e072ac910631d343038ce158a6de7d4f92
Jaroslavovich <>
2022-07-12 06:20:48 +0000

    Initial commit

---
+++ XMLReader.hpp
@@ -0,0 +1,25 @@
+#include "Section.hpp" // вообще порядок инклудов странный, что я даже сам толком не понимаю его. Но обычно системные инклуды помещают в вверх, а свои уже потом
+
+
+class XMLReader
+{
+public: 
+XMLReader(const std::string& filePath); 
+Section getRoot();
+
+private:
+std::string m_line;
+std::string m_tagName; 
+std::string m_value;
+std::vector<std::string> m_lines; 
+std::ifstream m_file;
+
+bool isClosingTag(const std::string& line) const; 
+bool isOpeningTag(const std::string& line) const; 
+bool isTabulation(const std::string& line) const;
+bool isValueInLine(const std::string& line) const;
+bool isChild(const std::string& line, const std::string& childLine) const;
+void fillChilds(Section& root, const std::string& line, unsigned index); 
+unsigned countTabulations(const std::string& line) const;
+Section getSectionFromLine(const std::string& line) const;
+};
